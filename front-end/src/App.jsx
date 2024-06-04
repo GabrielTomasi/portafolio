e05563d20664a0f760fb4345ccf517d172e7cae5
GabiTom <gabrieltomasi22@gmail.com>
2024-06-04 12:52:12 -0300

    ADD react router dom whit routes

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,8 +1,26 @@
+import { Route, Routes } from "react-router-dom"
 
-function App() {
+
+import Home from "./components/Home/Home.jsx"
+import Contact from "./components/Contact/Contact.jsx"
+import Stack from "./components/Stack/Stack.jsx"
+import Proyects from "./components/Proyects/Proyects.jsx"
+import NavBar from "./components/NavBar/NavBar.jsx"
+
+const App = () => {
   return (
+      <>
       <div>
+        <NavBar />
       </div>
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/stack"} element={<Stack />} />
+        <Route path={"/proyects"} element={<Proyects />} />
+        <Route path={"/contact"} element={<Contact />} />
+
+      </Routes>
+      </>
 
   )
 }
