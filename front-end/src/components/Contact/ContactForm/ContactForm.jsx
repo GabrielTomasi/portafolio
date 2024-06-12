a380e47d2f0cccc0a356a2d132c3aaa6304cc5e2
GabiTom <gabrieltomasi22@gmail.com>
2024-06-12 13:59:04 -0300

    Form functions

--- front-end/src/components/Contact/ContactForm/ContactForm.jsx
+++ front-end/src/components/Contact/ContactForm/ContactForm.jsx
@@ -2,7 +2,42 @@ import style from "./ContactForm.module.css";
 import instagram from "../../../assets/iconos/redes/instagram.png";
 import linkedin from "../../../assets/iconos/redes/linkedin.png";
 import github from "../../../assets/iconos/redes/github.png";
+import { useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+
 const ContactForm = () => {
+ const {VITE_SERVICE_ID, VITE_TEMPLATE_ID, VITE_PUBLIC_KEY}=import.meta.env
+  const formu = useRef();
+  const [formInfo, setFormInfo] = useState({
+    name: "",
+    lastName: "",
+    email: "",
+    message: "",
+  });
+
+
+  const handleFormInfo = (event) => {
+    const { name, value } = event.target;
+    setFormInfo({ ...formInfo,[name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    emailjs
+      .sendForm(VITE_SERVICE_ID, VITE_TEMPLATE_ID, formu.current, {
+        publicKey: VITE_PUBLIC_KEY,
+      })
+      .then(
+        (data) => {
+          console.log("success", data.text);
+        },
+        (error) => {
+          console.log("error: ", error.text);
+        }
+      );
+  };
+  console.log(formInfo);
   return (
     <div className={style.container}>
       <div className={style.headerContainer}>
@@ -16,22 +51,43 @@ const ContactForm = () => {
           <img src={github} alt="github" />
         </div>
       </div>
-      <form className={style.formContainer}>
+      <form className={style.formContainer} onSubmit={handleSubmit} ref={formu}>
         <label>
           <span>Nombre</span>
-          <input type="text" className={style.input} placeholder="Tu nombre"/>
+          <input
+            type="text"
+            name="name"
+            className={style.input}
+            placeholder="Tu nombre"
+            onChange={handleFormInfo}
+          />
         </label>
         <label>
           <span>Apellido</span>
-          <input className={style.input} placeholder="Tu apellido"/>
+          <input
+            className={style.input}
+            name="lastName"
+            placeholder="Tu apellido"
+            onChange={handleFormInfo}
+          />
         </label>
         <label>
           <span>Correo electrónico</span>
-          <input className={style.input}placeholder="example@example.com"/>
+          <input
+            className={style.input}
+            name="email"
+            placeholder="example@example.com"
+            onChange={handleFormInfo}
+          />
         </label>
         <label>
           <span>Tu mensaje</span>
-          <textarea className={style.textarea} placeholder="Tu mensaje"/>
+          <textarea
+            className={style.textarea}
+            name="message"
+            placeholder="Tu mensaje"
+            onChange={handleFormInfo}
+          />
         </label>
         <button type="submit">Enviar</button>
       </form>
